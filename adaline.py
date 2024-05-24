7c92c17df7c9e914e92e400d51d819b784fe8c65
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2024-05-24 23:13:57 +0800

    I think I fixed it?!

--- adaline.py
+++ adaline.py
@@ -4,39 +4,71 @@ import pandas as pd
 
 rng = np.random.default_rng()
 
+# Adaline - Adaptive Linear Neuron Classifier
+# Net input function -> Activation function -> Update Weights
+# loss function = 1/nΣ(y-y^)^2 | MSE in this case
+# ∂l/∂w = -2/nΣ(y-y^)*xi
+# ∂l/∂b = -2/nΣ(y-y^)
+# δw = -η * ∂L/∂w
+# δb = -η * ∂L/∂b
+ 
+
 class Adaline(object):
     
-    def __init__(self):
+    def __init__(self, x, y):
         #  Having it on random does change the learning process
-        self.eta = 0.01 
+        self.eta = 0.0001 
         #self.weights = np.zeros(units) Boundary wont change bcz the weights are all 0
         self.bias = np.float64(0.)
+        self.X = x
+        self.y = y
         self.losses = []
-
-    def fit(self, x, y, epochs):
         self.weights = rng.normal(loc=0.0, scale=0.01, size=x.shape[1])
+        
+    def fit(self, epochs):
 
         for _ in range(epochs):
-            lossVal = 0 # will hold the amount of errors in each epoch
-            update = 0
+            change_w = -self.eta * self.calculate_gradient_descent_w(self.X, self.y)
+            change_b = -self.eta * self.calculate_gradient_descent_b(self.X, self.y)
             
-            for xi1, target1 in zip(x,y):
-              update += (-2*(target1 - (np.dot(self.weights,xi1)+self.bias)))/len(y)
-      
-              loss = (update/-1)**2
-              update = -self.eta*update
+            self.weights = self.weights + change_w
+            self.b = self.bias + change_b
+            loss = self.calculate_loss(self.X, self.y)
+            self.losses.append(loss)
 
-              self.weights += update*xi1 # The rate of change is directly propotionate to the xi
-              self.bias += update
-
-              # This will update the error to the loss, until the final loop of xi will set the final loss value - To lazy to do it efficiently 
-              lossVal = loss
-            self.losses.append(lossVal) 
-                
         return self
     
-    def net_input(self, x):
-        return np.dot(x, self.weights) + self.bias
+    def net_input(self, xi):
+        return np.dot(xi, self.weights) + self.bias
+    
+    def calculate_loss(self, x, y):
+        loss = 0
+        for xi, target in zip(x,y):
+            z = self.net_input(xi)
+            loss_i = (target - z)**2
+            loss += loss_i
+            
+        return loss/x.shape[0]
+        
+    def calculate_gradient_descent_w(self, x, y):
+        loss = 0
+        for xi, target in zip(x,y):
+            z = self.net_input(xi)
+            for xji in xi:
+                loss_i = (target - z) * xji
+                loss += loss_i
+        gradient_descent = -2 * (loss/len(x))
+        return gradient_descent
+
+            
+    def calculate_gradient_descent_b(self, x, y):
+        loss = 0
+        for xi, target in zip(x,y):
+            z = self.net_input(xi)
+            loss_i = (target - z)
+            loss += loss_i
+        gradient_descent = -2 * (loss/len(x)) 
+        return gradient_descent
     
     def predict(self, x):
         return np.where(self.net_input(x) >= 0.0, 1, 0)
